3f8f8d78b719eb0f56971bad22eb943dd532fadf
Lucas Zluhan <lszluhan@hotmail.com>
2022-12-18 12:14:59 -0300

    add theme and type

--- src/screens/Groups/style.ts
+++ src/screens/Groups/style.ts
@@ -1,13 +1,8 @@
+import { theme } from '@theme/index'
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #000;
-  align-items: center;
-  justify-content: center;
-`
-
-export const Title = styled.Text`
-  color: #fff;
-  font-size: 32px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+  padding: 24px;
 `
