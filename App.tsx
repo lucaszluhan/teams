4f44b1ca0cd18495c06d0b8ca5e6d3d97d332df7
Lucas Zluhan <lszluhan@hotmail.com>
2022-12-18 12:34:24 -0300

    add custom fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { Groups } from '@screens/Groups'
 import { ThemeProvider } from 'styled-components'
 import { theme } from '@theme/index'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { ActivityIndicator } from 'react-native'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
-    <ThemeProvider theme={theme}>
-      <Groups />
-    </ThemeProvider>
+    <ThemeProvider theme={theme}>{fontsLoaded ? <Groups /> : <ActivityIndicator />}</ThemeProvider>
   )
 }
