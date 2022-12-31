65b18e91fdcea620870e895aab315ceb112ef9bd
Lucas Zluhan <lszluhan@hotmail.com>
2022-12-31 15:54:48 -0300

    add routes

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
-import { Groups, NewGroup, Players } from '@screens/index'
 import { ThemeProvider } from 'styled-components'
 import { theme } from '@theme/index'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Loading } from '@components/index'
 import { StatusBar } from 'react-native'
+import { Routes } from '@routes/index'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar barStyle={'light-content'} backgroundColor={'transparent'} translucent />
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   )
 }
