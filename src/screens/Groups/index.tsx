a0cc4c02cecdcbc0dd589fa6738cd08de2a621a4
Lucas Zluhan <lszluhan@hotmail.com>
2022-12-31 16:09:00 -0300

    add navigation type

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,16 @@
 import { Button, GroupCard, Header, Highlight, ListEmpty } from '@components/index'
+import { useNavigation } from '@react-navigation/native'
 import { useState } from 'react'
 import { FlatList } from 'react-native'
 import { Container } from './style'
 
 export function Groups() {
   const [groups, setGroups] = useState([])
+  const navigation = useNavigation()
+
+  function handleNewGroup() {
+    navigation.navigate('players', { group: 'Arezzo' })
+  }
 
   return (
     <Container>
@@ -17,7 +23,7 @@ export function Groups() {
         ListEmptyComponent={() => <ListEmpty message='Que tal cadastrar uma turma?' />}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
       />
-      <Button title='Criar nova turma' />
+      <Button title='Criar nova turma' onPress={handleNewGroup} />
     </Container>
   )
 }
